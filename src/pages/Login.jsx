a1aa10d8be3ac766c34b9aeb698a31b7aaa60fa6
franklinnnn <franklin.assa@gmail.com>
2023-03-19 20:56:26 -0700

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { REDIRECT_URI, AUTH_URL, RESPONSE_TYPE, SCOPE } from "../util/spotify";
 import { BsSpotify } from "react-icons/bs";
 
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-2  h-[100vh]">
       <h1 className="text-2xl mb-2">Copy of Receiptify</h1>
       <div>
         <a
-          href={`${AUTH_URL}?client_id=${
-            import.meta.env.VITE_CLIENT_ID
-          }&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
+          href={`${AUTH_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
           className="flex justify-center items-center gap-2 p-2 rounded-sm bg-slate-500 hover:bg-slate-600"
         >
           Login to
