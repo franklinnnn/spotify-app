b43e63ed634a08b4c25515e951eb9119d29a789c
franklinnnn <franklin.assa@gmail.com>
2023-05-10 19:23:53 -0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
           />
         </div>
 
-        <span className="text-[3rem] font-disp font-bold tracking-wide uppercase">
+        <span className="text-[3rem] font-disp tracking-wide uppercase">
           Decksio
         </span>
       </h1>
